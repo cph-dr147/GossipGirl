6689f815c2d4cdcfe377151959c6df1ec6b802a9
cph-dr147 <dana.albidani@gmail.com>
2025-12-07 15:38:44 +0100

    sign up almost done

---
+++ src/main/java/com/example/gossipgirl/GossipFeedController.java
@@ -0,0 +1,5 @@
+package com.example.gossipgirl;
+
+public class GossipFeedController{
+
+}
--- src/main/java/com/example/gossipgirl/Message.java
+++ src/main/java/com/example/gossipgirl/Message.java
@@ -1,3 +1,44 @@
-public class Message{
+package com.example.gossipgirl;
 
+import java.util.Date;
+
+public class Message{
+    String originalText;
+    String rephrasedText;
+    Date timestamp;
+    final String author = "anonymous";
+
+    public Message(String originalText, String rephrasedText,  Date timestamp,  String author  ) {
+        this.originalText = originalText;
+        this.rephrasedText = rephrasedText;
+        this.timestamp = timestamp;
+    }
+
+    public String getOriginalText() {
+        return originalText;
+    }
+
+    public void setOriginalText(String originalText) {
+        this.originalText = originalText;
+    }
+
+    public String getRephrasedText() {
+        return rephrasedText;
+    }
+
+    public void setRephrasedText(String rephrasedText) {
+        this.rephrasedText = rephrasedText;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
